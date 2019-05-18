0068f84f63246fcb11730e05810da7a1d5c53225
Michał Suliborski <msuliborski@gmail.com>
2019-05-18 15:49:17 +0200

    ea

--- View/src/main/java/pl/comp/view/MainView.java
+++ View/src/main/java/pl/comp/view/MainView.java
@@ -1,7 +1,11 @@
 package pl.comp.view;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.adapter.JavaBeanIntegerProperty;
+import javafx.beans.property.adapter.JavaBeanIntegerPropertyBuilder;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -13,12 +17,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import javafx.util.StringConverter;
+import javafx.util.converter.IntegerStringConverter;
+import javafx.util.converter.NumberStringConverter;
 import pl.comp.model.BacktrackingSudokuSolver;
 import pl.comp.model.SudokuBoard;
 import pl.comp.model.SudokuBoardDaoFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -39,7 +48,12 @@ public class MainView implements Initializable {
     public MenuItem load;
     public Menu language;
     private List<List<TextField>> boardTextFields = new ArrayList<>();
+//    private JavaBeanIntegerProperty[][] integerProperties = new JavaBeanIntegerProperty[9][9];
+//    private JavaBeanIntegerPropertyBuilder builder = JavaBeanIntegerPropertyBuilder.create();
+//    private StringConverter converter = new IntegerStringConverter();
     private static boolean isEnglish = true;
+
+    int gunwo = 9;
 
     //private
 
@@ -65,7 +79,7 @@ public class MainView implements Initializable {
                 emptyTextField.setPrefWidth(100);
                 emptyTextField.setFont(Font.font("Verdana", 36));
                 emptyTextField.setTextFormatter(new TextFormatter<String>((TextFormatter.Change change) -> {
-                    if(change.getText().matches("[0-9]*") && !(change.getText().length() > 1)) {
+                    if(change.getText().matches("[0-9]*") && !(change.getControlNewText().length() > 1)) {
                         return change;
                     } else {
                         return null;
@@ -73,19 +87,51 @@ public class MainView implements Initializable {
                 }));
 
 
+                int finalX = x;
+                int finalY = y;
+                emptyTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+                    sudokuBoard.setFieldValue(finalY, finalX, Integer.parseInt(newValue));//.g.get(x).add(emptyTextField);
+                    System.out.println(sudokuBoard.toString());
+                });
 
-//
-//                emptyTextField.textProperty().addListener(new ChangeListener<>() {
-//                    @Override
-//                    public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
-//                        sudokuBoard.g.get(x).add(emptyTextField);
-//                        System.out.println("textField old val: " + arg1);
-//                        System.out.println("textField new val: " + arg2);
-//                        System.out.println();
+//                emptyTextField.textProperty().bindBidirectional((Property<String>) sudokuBoard.getField(x, y));
+
+                Bindings.bindBidirectional(emptyTextField.textProperty(), startPageProperty, new NumberStringConverter());
+//                try {
+//                    integerProperties[y][x] = builder.bean(sudokuBoard.getField(y, x)).name("value").build();
+//                    emptyTextField.textProperty().bindBidirectional(integerProperties[y][x], converter);
+////                    emptyTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+////                        System.out.println(sudokuBoard.toString());
+//////                        if (newValue.equals("0")) {
+//////                            textFields[j2][i2].setText("");
+//////                        } else if (newValue.matches("\\d+") && (Integer.valueOf(newValue) > 9 || Integer.valueOf(newValue) < 0)) {
+//////                            textFields[j2][i2].setText(oldValue);
+//////                        }
+////                    });
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                }
+
+//                try {
+//                    integerProperties[y][x] = builder.bean(sudokuBoard.getField(y, x)).name("value").build();
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                }
+//                emptyTextField.textProperty().bindBidirectional(integerProperties[y][x], converter);
+//                integerProperties[y][x].addListener((observable, oldValue, newValue) -> {
+//                    if (newValue.equals("0")) {
+//                        emptyTextField.setText("");
+//                        if (sudokuBoard.isFieldDefault(finalX, finalY)) {
+//                            boardTextFields.get(finalX).get(finalY).setStyle("-fx-text-fill: grey;");
+//                            boardTextFields.get(finalX).get(finalY).setEditable(false);
+//                        }
+//                    } else if ((newValue.intValue() > 9 || newValue.intValue() < 0)) {
+//                        emptyTextField.setText(String.valueOf(oldValue.intValue()));
 //                    }
+
 //                });
 //
-//                model.firstProperty().addListener(new ChangeListener<String>() {
+//                sudokuBoard.firstProperty().addListener(new ChangeListener<String>() {
 //                    @Override
 //                    public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
 //                        System.out.println("model old val: " + arg1);
@@ -149,34 +195,37 @@ public class MainView implements Initializable {
     }
 
     public void verify(ActionEvent actionEvent) {
-        if (sudokuBoard != null) {
-            updateSudokuBoard();
-            if (sudokuBoard.verify() && !sudokuBoard.areThereZeros()) {
-                if (isEnglish)
-                    verifyButton.setText("CORRECT!");
-                else
-                    verifyButton.setText("DOBRZE!");
-                verifyButton.setTextFill(Color.GREEN);
-            } else if (sudokuBoard.verify()) {
-                if (isEnglish)
-                    verifyButton.setText("NO LOGIC ERRORS!");
-                else
-                    verifyButton.setText("BRAK BŁĘDÓW!");
-                verifyButton.setTextFill(Color.BLUE);
-            } else {
-                if (isEnglish)
-                    verifyButton.setText("WRONG!");
-                else
-                    verifyButton.setText("ŹLE!");
-                verifyButton.setTextFill(Color.RED);
-            }
-        } else {
-            if (isEnglish)
-                verifyButton.setText("START NEW GAME FROM THE MENU ABOVE!");
-            else
-                verifyButton.setText("ZACZNIJ NOWĄ GRĘ Z MENU NA GÓRZE!");
-            verifyButton.setTextFill(Color.RED);
-        }
+        integerProperties[2][2].set(3);
+
+        System.out.println(sudokuBoard.toString());
+//        if (sudokuBoard != null) {
+//            updateSudokuBoard();
+//            if (sudokuBoard.verify() && !sudokuBoard.areThereZeros()) {
+//                if (isEnglish)
+//                    verifyButton.setText("CORRECT!");
+//                else
+//                    verifyButton.setText("DOBRZE!");
+//                verifyButton.setTextFill(Color.GREEN);
+//            } else if (sudokuBoard.verify()) {
+//                if (isEnglish)
+//                    verifyButton.setText("NO LOGIC ERRORS!");
+//                else
+//                    verifyButton.setText("BRAK BŁĘDÓW!");
+//                verifyButton.setTextFill(Color.BLUE);
+//            } else {
+//                if (isEnglish)
+//                    verifyButton.setText("WRONG!");
+//                else
+//                    verifyButton.setText("ŹLE!");
+//                verifyButton.setTextFill(Color.RED);
+//            }
+//        } else {
+//            if (isEnglish)
+//                verifyButton.setText("START NEW GAME FROM THE MENU ABOVE!");
+//            else
+//                verifyButton.setText("ZACZNIJ NOWĄ GRĘ Z MENU NA GÓRZE!");
+//            verifyButton.setTextFill(Color.RED);
+//        }
     }
 
     public void saveGame() {
