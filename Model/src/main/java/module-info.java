a9dee7017f86d86a2fb3a876d73a6700cfddde6f
jedrek <jedrekszor@wp.pl>
2019-06-03 15:50:23 +0200

    fshfksjd

--- Model/src/main/java/module-info.java
+++ Model/src/main/java/module-info.java
@@ -2,7 +2,6 @@ open module ModelProject {
     exports pl.comp.model;
     exports pl.comp.model.sudoku;
     exports pl.comp.model.solver;
-    exports pl.comp.model.lang.Re;
     exports pl.comp.model.dao;
     requires org.apache.commons.lang3;
     requires java.logging;
